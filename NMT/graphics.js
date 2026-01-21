36207cf86d1d852f37bb0df323afaccb5093398f
MD3728 <mdong9773@gmail.com>
2026-01-21 10:01:56 -0500

    2026 Rebalancing I

--- NMT/graphics.js
+++ NMT/graphics.js
@@ -2,13 +2,24 @@
 
 // Developer Shortcut Methods
 function rich() {
-  sun = 99999
-  money = 99999
+  sun = 99999;
+  money = 99999;
   for (currentPacket of allPackets) {
-    currentPacket.recharge = 0
+    currentPacket.recharge = 0;
   }
   for (currentZombie of allZombies) {
     currentZombie.health = 0;
+  }
+}
+
+function devMode() {
+  money = 20000;
+  unlockedLevels = ["l1", "l2", "l3", "l4", "l5", "l6", "l7", "l8", "l9", "l10",
+  "l11", "l12", "l13", "l14", "l15", "l16", "l17", "l18", "l19", "l20",
+  "l21", "l22", "l23", "l24", "l25", "l26", "l27", "l28", "l29", "l30",
+  "l31", "l32", "l33", "l34", "l35", "l36", "m1", "m2", "m3", "m4", "m5", "m6", "m7", "m8", "m9", "m10", "m11", "m12", "m13"];
+  for (let a = 0; a < 50; a++) {
+    unlockedPackets.push(a);
   }
 }
 
